5cbb935d29dee2046af65314b32e60b417cd92aa
lc <1119702379@qq.com>
2019-05-16 20:31:32 +0800

    add LR

---
+++ Logistic_Regression/README.md
@@ -0,0 +1,12 @@
+### 项目案例1: 使用 Logistic 回归在简单数据集上的分类
+
+```
+python logistic_1.py
+```
+
+### 项目案例2: 从疝气病症预测病马的死亡率
+
+```
+python logistic_2.py
+```
+
---
+++ Logistic_Regression/logistic_2.py
@@ -0,0 +1,121 @@
+from __future__ import print_function
+from numpy import *
+
+
+# --------------------------------------------------------------------------------
+# 从疝气病症预测病马的死亡率
+# 分类函数，根据回归系数和特征向量来计算 Sigmoid的值
+
+# sigmoid跳跃函数
+def sigmoid(inX):
+    # Tanh是Sigmoid的变形，与 sigmoid 不同的是，tanh 是0均值的。因此，实际应用中，tanh 会比 sigmoid 更好。
+    if inX>=0:
+        return 1.0/(1+exp(-inX))
+    else:
+        return exp(inX)/(1+exp(inX))
+
+
+def classifyVector(inX, weights):
+    '''
+    Desc: 
+        最终的分类函数，根据回归系数和特征向量来计算 Sigmoid 的值，大于0.5函数返回1，否则返回0
+    Args:
+        inX -- 特征向量，features
+        weights -- 根据梯度下降/随机梯度下降 计算得到的回归系数
+    Returns:
+        如果 prob 计算大于 0.5 函数返回 1
+        否则返回 0
+    '''
+    prob = sigmoid(sum(inX * weights))
+    if prob > 0.5: return 1.0
+    else: return 0.0
+
+
+# 随机梯度下降算法（随机化）
+def stocGradAscent1(dataMatrix, classLabels, numIter=150):
+    '''
+    Desc:
+        改进版的随机梯度下降，使用随机的一个样本来更新回归系数
+    Args:
+        dataMatrix -- 输入数据的数据特征（除去最后一列数据）
+        classLabels -- 输入数据的类别标签（最后一列数据）
+        numIter=150 --  迭代次数
+    Returns:
+        weights -- 得到的最佳回归系数
+    '''
+    m, n = shape(dataMatrix)
+    weights = ones(n)  # 创建与列数相同的矩阵的系数矩阵，所有的元素都是1
+    # 随机梯度, 循环150,观察是否收敛
+    for j in range(numIter):
+        # [0, 1, 2 .. m-1]
+        dataIndex = range(m)
+        for i in range(m):
+            # i和j的不断增大，导致alpha的值不断减少，但是不为0
+            alpha = 4 / (
+                1.0 + j + i
+            ) + 0.0001  # alpha 会随着迭代不断减小，但永远不会减小到0，因为后边还有一个常数项0.0001
+            # 随机产生一个 0～len()之间的一个值
+            # random.uniform(x, y) 方法将随机生成下一个实数，它在[x,y]范围内,x是这个范围内的最小值，y是这个范围内的最大值。
+            randIndex = int(random.uniform(0, len(dataIndex)))
+            # sum(dataMatrix[i]*weights)为了求 f(x)的值， f(x)=a1*x1+b2*x2+..+nn*xn
+            h = sigmoid(sum(dataMatrix[dataIndex[randIndex]] * weights))
+            error = classLabels[dataIndex[randIndex]] - h
+            # print weights, '__h=%s' % h, '__'*20, alpha, '__'*20, error, '__'*20, dataMatrix[randIndex]
+            weights = weights + alpha * error * dataMatrix[dataIndex[randIndex]]
+            #del (dataIndex[randIndex])
+    return weights
+
+
+# 打开测试集和训练集,并对数据进行格式化处理
+def colicTest():
+    '''
+    Desc:
+        打开测试集和训练集，并对数据进行格式化处理
+    Args:
+        None
+    Returns:
+        errorRate -- 分类错误率
+    '''
+    frTrain = open('./data/horseColicTraining.txt')
+    frTest = open('./data/horseColicTest.txt')
+    trainingSet = []
+    trainingLabels = []
+    # 解析训练数据集中的数据特征和Labels
+    # trainingSet 中存储训练数据集的特征，trainingLabels 存储训练数据集的样本对应的分类标签
+    for line in frTrain.readlines():
+        currLine = line.strip().split('\t')
+        lineArr = []
+        for i in range(21):
+            lineArr.append(float(currLine[i]))
+        trainingSet.append(lineArr)
+        trainingLabels.append(float(currLine[21]))
+    # 使用 改进后的 随机梯度下降算法 求得在此数据集上的最佳回归系数 trainWeights
+    trainWeights = stocGradAscent1(array(trainingSet), trainingLabels, 500)
+    errorCount = 0
+    numTestVec = 0.0
+    # 读取 测试数据集 进行测试，计算分类错误的样本条数和最终的错误率
+    for line in frTest.readlines():
+        numTestVec += 1.0
+        currLine = line.strip().split('\t')
+        lineArr = []
+        for i in range(21):
+            lineArr.append(float(currLine[i]))
+        if int(classifyVector(array(lineArr), trainWeights)) != int(
+                currLine[21]):
+            errorCount += 1
+    errorRate = (float(errorCount) / numTestVec)
+    print("the error rate of this test is: %f" % errorRate)
+    return errorRate
+
+
+# 调用 colicTest() 10次并求结果的平均值
+def multiTest():
+    numTests = 10
+    errorSum = 0.0
+    for k in range(numTests):
+        errorSum += colicTest()
+    print("after %d iterations the average error rate is: %f" % (numTests, errorSum / float(numTests)))
+
+
+if __name__ == "__main__":
+    multiTest()
